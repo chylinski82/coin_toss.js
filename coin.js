fda4f4fefd3d6a346a33add7ee48c863aa9f79ff
“[Krzysztof <[k.chylinski82@gmail.com]>
2022-03-25 22:29:52 +0000

    ammending tails outcome

--- coin.js
+++ coin.js
@@ -9,7 +9,7 @@ toss = (tries) => {
         let coin = Math.floor(Math.random() * 2);
         if(coin === 0) {
             tails = tails + 1;
-            portfolio = portfolio * 0.4;
+            portfolio = portfolio * 0.6;
         } else {
             heads = heads + 1;
             portfolio = portfolio * 1.5;
@@ -20,7 +20,7 @@ toss = (tries) => {
 Your current portfolio is: ${portfolio.toFixed(4)} $oy pounds`);   
 }
 
-toss(50)
+toss(100)
 
 
 
