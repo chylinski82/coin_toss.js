f514c13874c91de8aeecb0d3fbc380e754be6832
“[Krzysztof <[k.chylinski82@gmail.com]>
2022-03-26 11:20:54 +0000

    added size variable which allows to bet a certain % of portfolio each game

--- coin.js
+++ coin.js
@@ -1,30 +1,38 @@
 let portfolio = 1000;
 
 
-toss = (tries) => {
+toss = (tries, size=1) => {
     let n = 0;
     let heads = 0;
     let tails = 0;
 
-    //console.log(`your starting portfolio is: ${portfolio} $oy pounds`);
+    //console.log(`your starting portfolio is: ${portfolio} $oy pounds and your betting ${size * 100}% of your portfolio each time`);
 
     while (n < tries) {
         let coin = Math.floor(Math.random() * 2);
-        if(coin === 0) {
+        if (coin === 0) {
             tails = tails + 1;
-            portfolio = portfolio * 0.6;
+            if (size === 1) {
+                portfolio = portfolio * 0.6
+            } else {
+                portfolio = portfolio - portfolio * size * 0.4
+            }               
         } else {
             heads = heads + 1;
-            portfolio = portfolio * 1.5;
-        }
+            if (size === 1) {
+                portfolio = portfolio * 1.5
+            } else {
+                portfolio = portfolio + portfolio * size * 0.5
+            }
+        }           
         n++
     }
-    return portfolio
     //console.log(`after ${tries} tries you got ${heads} heads and ${tails} tails. 
-//Your current portfolio is: ${portfolio.toFixed(4)} $oy pounds`);   
+    //Your current portfolio is: ${portfolio.toFixed(4)} $oy pounds`);
+    return portfolio 
 }
 
-//toss(100)
+//toss(100, 0.1)
 
 multiple_games = (games) => {
     
@@ -33,7 +41,7 @@ multiple_games = (games) => {
     let l = 0; //number of losses
     let e = 0; //number of break evens
     while (g < games) {
-        toss(100);
+        toss(100, 0.25);
         if(portfolio > 1000) {
             w = w + 1
         } else if(portfolio < 1000) {
