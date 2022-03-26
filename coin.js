58df65554a3cd062761718e1ca8aafcfc79ffd6a
“[Krzysztof <[k.chylinski82@gmail.com]>
2022-03-26 10:26:20 +0000

    added multiple_games function to calculate a probability of winning a set of coin tosses

--- coin.js
+++ coin.js
@@ -1,10 +1,13 @@
+let portfolio = 1000;
+
 
 toss = (tries) => {
-    let portfolio = 1000;
+    let n = 0;
     let heads = 0;
     let tails = 0;
-    let n = 0;
-    console.log(`your starting portfolio is: ${portfolio} $oy pounds`);
+
+    //console.log(`your starting portfolio is: ${portfolio} $oy pounds`);
+
     while (n < tries) {
         let coin = Math.floor(Math.random() * 2);
         if(coin === 0) {
@@ -16,12 +19,35 @@ toss = (tries) => {
         }
         n++
     }
-    console.log(`after ${tries} tries you got ${heads} heads and ${tails} tails. 
-Your current portfolio is: ${portfolio.toFixed(4)} $oy pounds`);   
+    return portfolio
+    //console.log(`after ${tries} tries you got ${heads} heads and ${tails} tails. 
+//Your current portfolio is: ${portfolio.toFixed(4)} $oy pounds`);   
 }
 
-toss(100)
+//toss(100)
 
+multiple_games = (games) => {
+    
+    let g = 0; //number of games
+    let w = 0; //number of wins
+    let l = 0; //number of losses
+    let e = 0; //number of break evens
+    while (g < games) {
+        toss(100);
+        if(portfolio > 1000) {
+            w = w + 1
+        } else if(portfolio < 1000) {
+            l = l + 1
+        } else {
+            e = e + 1
+        }
+        g++;
+        portfolio = 1000; //need to reset portfolio to starting value.
+    }
+    console.log(`of ${games} games, you won ${w} times, you lost ${l} times and ${e} times you ended breaking even`)
+}
+
+multiple_games(10000)
 
 
 
